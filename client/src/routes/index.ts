03689acf1e8055dba86d9c595b15dd4c1341443c
Daniel Shin <140947149+danielkshin@users.noreply.github.com>
2025-01-20 14:44:35 -0800

    Fix frontend styling and change routes structure

---
+++ client/src/routes/index.ts
@@ -0,0 +1,3 @@
+export { default as HomePage } from 'routes/HomePage/HomePage';
+export { default as LoginPage } from 'routes/LoginPage/LoginPage';
+export { default as ExplorePage } from 'routes/ExplorePage/ExplorePage';
