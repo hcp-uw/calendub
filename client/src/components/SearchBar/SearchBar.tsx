6dc197ceba014a892c67e91d8d69403de3514a21
Daniel Shin <140947149+danielkshin@users.noreply.github.com>
2025-01-22 23:09:36 -0800

    Searchbar refinements

--- client/src/components/SearchBar/SearchBar.tsx
+++ client/src/components/SearchBar/SearchBar.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, FocusEvent, useRef, useState } from 'react';
+import { ChangeEvent, FocusEvent, useEffect, useRef, useState } from 'react';
 import './SearchBar.css';
 import { FaSearch } from 'react-icons/fa';
 import { useNavigate } from 'react-router-dom';
@@ -6,6 +6,7 @@ import { useNavigate } from 'react-router-dom';
 const SearchBar = () => {
   const [searchValue, setSearchValue] = useState('');
   const [showSuggestions, setShowSuggestions] = useState(false);
+  const [activeIndex, setActiveIndex] = useState(-1);
 
   // Hardcoded values for now
   const usernames = [
@@ -31,13 +32,51 @@ const SearchBar = () => {
   const handleSuggestionClick = (suggestion: string) => {
     setSearchValue(suggestion);
     setShowSuggestions(false);
+    setActiveIndex(-1);
   };
+
+  useEffect(() => {
+    handleSearchClick();
+  }, [searchValue]);
 
   const handleOutsideClick = (e: FocusEvent<HTMLInputElement>) => {
     if (!e.relatedTarget?.classList.contains('suggestion-item')) {
       setShowSuggestions(false);
+      setActiveIndex(-1);
     }
   };
+
+  const handleSearchClick = () => {
+    if (usernames.includes(searchValue)) {
+      navigate(`/profile/${searchValue}`);
+    }
+  };
+
+  const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (!showSuggestions) return;
+
+    switch (e.key) {
+      case 'ArrowDown':
+        e.preventDefault();
+        setActiveIndex((prevIndex) =>
+          prevIndex < filteredSuggestions.length - 1 ? prevIndex + 1 : prevIndex
+        );
+        break;
+      case 'ArrowUp':
+        e.preventDefault();
+        setActiveIndex((prevIndex) => (prevIndex > 0 ? prevIndex - 1 : -1));
+        break;
+      case 'Enter':
+        if (activeIndex >= 0) {
+          handleSuggestionClick(filteredSuggestions[activeIndex]);
+        }
+        break;
+      case 'Escape':
+        setShowSuggestions(false);
+        break;
+    }
+  };
+
   const suggestionsRef = useRef<HTMLDivElement>(null);
 
   const navigate = useNavigate();
@@ -52,17 +91,20 @@ const SearchBar = () => {
           onChange={handleSearchChange}
           onFocus={() => setShowSuggestions(true)}
           onBlur={handleOutsideClick}
+          onKeyDown={handleKeyPress}
         ></input>
-        <button onClick={() => navigate(`/profile/${searchValue}`)}>
+        <button onClick={handleSearchClick}>
           <FaSearch size={14} />
         </button>
       </div>
       {showSuggestions && searchValue && filteredSuggestions.length > 0 && (
         <div className="suggestions-list" ref={suggestionsRef}>
-          {filteredSuggestions.map((suggestion) => (
+          {filteredSuggestions.map((suggestion, index) => (
             <div
               key={suggestion}
-              className="suggestion-item"
+              className={`suggestion-item ${
+                index === activeIndex ? 'active' : ''
+              }`}
               onClick={() => handleSuggestionClick(suggestion)}
               tabIndex={0}
             >
