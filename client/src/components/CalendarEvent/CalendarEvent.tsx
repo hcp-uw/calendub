05b8885a70275c6c0e28b961949f7af5cf26a044
Daniel Shin <140947149+danielkshin@users.noreply.github.com>
2025-02-28 14:05:12 -0800

    Test popups

--- client/src/components/CalendarEvent/CalendarEvent.tsx
+++ client/src/components/CalendarEvent/CalendarEvent.tsx
@@ -1,3 +1,4 @@
+import { MouseEvent, useState } from 'react';
 import './calendarEvent.css';
 
 interface CalendarEventProps {
@@ -7,14 +8,50 @@ interface CalendarEventProps {
 }
 
 const CalendarEvent = (props: CalendarEventProps) => {
+  const [popup, setPopup] = useState({
+    show: false,
+    x: 0,
+    y: 0,
+    content: '',
+  });
+
+  const closePopup = () => {
+    setPopup({ ...popup, show: false });
+  };
+
+  const test = (e: MouseEvent<HTMLElement>) => {
+    const loc = e.currentTarget.getBoundingClientRect();
+    console.log(loc);
+    setPopup({
+      show: true,
+      x: loc.x + loc.width / 2,
+      y: loc.y + loc.height * 1.5,
+      content: 'This is a test popup',
+    });
+  };
+
   return (
-    <div
-      className="label"
-      style={{ backgroundColor: props.color }}
-      onClick={() => props.onClick()}
-    >
-      {props.name}
-    </div>
+    <>
+      <div
+        className="label"
+        style={{ backgroundColor: props.color }}
+        onClick={test}
+      >
+        {props.name}
+      </div>
+      {popup.show && (
+        <div
+          style={{
+            left: popup.x,
+            top: popup.y,
+          }}
+          className="popup card"
+        >
+          {popup.content}
+          <button onClick={closePopup}>Close</button>
+        </div>
+      )}
+    </>
   );
 };
 
