bfd82a0d41f1c93344c4da6053d2026b227ece6f
Daniel Shin <140947149+danielkshin@users.noreply.github.com>
2025-03-02 18:08:51 -0800

    Event details popup basic styling

--- client/src/components/EventDetails/EventDetails.tsx
+++ client/src/components/EventDetails/EventDetails.tsx
@@ -10,9 +10,8 @@ interface EventDetailsProps {
 const EventDetails = (props: EventDetailsProps) => {
   return (
     <div className="event-details card">
-      <h2>{props.selectedEvent.name}</h2>
-      <h4>{props.selectedEvent.time}</h4>
-      <p>{props.selectedEvent.type}</p>
+      <h1>{props.selectedEvent.name}</h1>
+      <h2>{props.selectedEvent.time}</h2>
       <p>{props.selectedEvent.location}</p>
       <p>{props.selectedEvent.description}</p>
       <button onClick={props.closeEventDetailsPopup}>Close</button>
